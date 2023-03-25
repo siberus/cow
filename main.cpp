ccce631a30ee685507a184f32eec3a5542163904
Vladislav <mr.siberus@yandex.ru>
2023-03-25 18:52:07 +0700

    Add coments

--- main.cpp
+++ main.cpp
@@ -6,15 +6,17 @@ using namespace std;
 
 int main()
 {
-    int n, temp, max, max_pos, index, rang;
+    int n, temp, max, max_pos, index, rang, result;
     vector<int> arr, rangs;
     bool flag = false;
+    //Ввод данных
     cin >> n;
     for (int i = 0; i < n; i++)
     {
         cin >> temp;
         arr.push_back(temp);
     }
+    //Поиск максимальноо результата
     max = arr.front();
     for (int i = 1; i < arr.size(); i++)
     {
@@ -23,46 +25,47 @@ int main()
             max_pos = i;
         }
     }
+    //Поиск результата по условию
     index = 0;
     for (int i = max_pos+1; i < arr.size() - 1; i++)
     {
-        
-            if ((arr.at(i) % 10 != 0) && (arr.at(i) % 5 == 0) && (arr.at(i) > arr.at(i + 1)))
+        if ((arr.at(i) % 10 != 0) && (arr.at(i) % 5 == 0) && (arr.at(i) > arr.at(i + 1)))
+        {
+            if (!flag)
             {
-                if (!flag)
-                {
-                    index = i;
-                    flag = true;
-                }
-                else
-                {
-                    if (arr.at(i) > arr.at(index))
-                        index = i;
-                }
+                index = i;
+                flag = true;
             }
-        
+            else
+            {
+                if (arr.at(i) > arr.at(index))
+                    index = i;
+            }
+        }    
     }
-    rang = 1;
+    //Вывод если значение не найдено
     if (!flag)
     {
         cout << "0";
         return 0;
     }
-    temp = arr.front();
-    for (int i = 0; i < arr.size(); i++) {
-        if (arr.at(i) != temp) {
-            rangs.push_back(temp);
-            temp = arr.at(i);
-        }
+    //Формирование массива различных результатов (удаление повторяющихся)
+    rang = 1;
+    result = arr.at(index);
+    for (int i = 0; i < arr.size()-1; i++) {
+        for (int j = 1; j < arr.size(); j++) {
+            if (arr.at(i) == arr.at(j)) {
+                arr.erase(arr.begin() + j);
+            }
+        }   
     }
-    rangs.push_back(temp);
-
-    for (int i = 0; i < rangs.size(); i++) {
-        if (rangs.at(i) > rangs.at(index)) {
+    //Поиск места
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr.at(i) > result) {
             rang++;
         }
     }
-
+    //Вывод результата
     cout << rang;
 
     return 0;
